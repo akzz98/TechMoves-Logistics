7f1a8e43a79db4008ead554db8368f116c5ff238
zulukho0 <zulukho0@gmail.com>
2026-04-22 10:05:36 +0200

    Add Client model with properties and navigation to Contract

---
+++ TechMoves Logistics/Models/Client.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+namespace TechMoves_Logistics.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, MaxLength(200)]
+        public string ContactDetails { get; set; } = string.Empty;
+
+        [Required, MaxLength(100)]
+        public string Region { get; set; } = string.Empty;
+
+        // Navigation property
+        public ICollection<Contract> Contracts { get; set; } = new List<Contract>();
+    }
+}
