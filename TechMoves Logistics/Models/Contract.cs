0ddf56f7ae27ed04c938bb7d68d950d4aff3e6af
zulukho0 <zulukho0@gmail.com>
2026-04-22 10:04:57 +0200

    Add Contract and ContractStatus models

---
+++ TechMoves Logistics/Models/Contract.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TechMoves_Logistics.Models.Enums;
+
+namespace TechMoves_Logistics.Models
+{
+    public class Contract
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ClientId { get; set; }
+
+        [ForeignKey("ClientId")]
+        public Client? Client { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public ContractStatus Status { get; set; } = ContractStatus.Draft;
+
+        [Required, MaxLength(200)]
+        public string ServiceLevel { get; set; } = string.Empty;
+
+        // PDF file path
+        public string? SignedAgreementPath { get; set; }
+
+        // Navigation property
+        public ICollection<ServiceRequest> ServiceRequests { get; set; } = new List<ServiceRequest>();
+    }
+}
---
+++ TechMoves Logistics/Models/Enums/ContractStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace TechMoves_Logistics.Models.Enums
+{
+    public enum ContractStatus
+    {
+        Draft,
+        Active,
+        Expired,
+        OnHold
+    }
+}
