ac7c9bece481f9a0c5a72e05ad37235a64ec6003
zulukho0 <zulukho0@gmail.com>
2026-04-22 10:09:52 +0200

    Add ClientRepository and IClientRepository interfaces with CRUD operations

---
+++ TechMoves Logistics/Repositories/ClientRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechMoves_Logistics.Data;
+using TechMoves_Logistics.Models;
+using TechMoves_Logistics.Repositories.Interfaces;
+
+namespace TechMoveLogistics.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Client>> GetAllAsync()
+            => await _context.Clients.ToListAsync();
+
+        public async Task<Client?> GetByIdAsync(int id)
+            => await _context.Clients
+                .Include(c => c.Contracts)
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+        public async Task AddAsync(Client client)
+        {
+            await _context.Clients.AddAsync(client);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Client client)
+        {
+            _context.Clients.Update(client);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+            if (client != null)
+            {
+                _context.Clients.Remove(client);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ TechMoves Logistics/Repositories/Interfaces/IClientRepository.cs
@@ -0,0 +1,13 @@
+﻿using TechMoves_Logistics.Models;
+
+namespace TechMoves_Logistics.Repositories.Interfaces
+{
+    public interface IClientRepository
+    {
+        Task<IEnumerable<Client>> GetAllAsync();
+        Task<Client?> GetByIdAsync(int id);
+        Task AddAsync(Client client);
+        Task UpdateAsync(Client client);
+        Task DeleteAsync(int id);
+    }
+}
