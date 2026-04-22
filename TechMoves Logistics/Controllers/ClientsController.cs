50e7eac73b8ecd5691ba23bd957bef1947cef9c9
zulukho0 <zulukho0@gmail.com>
2026-04-22 10:13:21 +0200

    Add ClientsController implementing CRUD operations for Client entities

---
+++ TechMoves Logistics/Controllers/ClientsController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechMoves_Logistics.Data;
+using TechMoves_Logistics.Models;
+using TechMoves_Logistics.Repositories.Interfaces;
+
+namespace TechMoves_Logistics.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private readonly IClientRepository _clientRepo;
+
+        public ClientsController(IClientRepository clientRepo)
+        {
+            _clientRepo = clientRepo;
+        }
+
+        // GET: Clients
+        public async Task<IActionResult> Index()
+        {
+            return View(await _clientRepo.GetAllAsync());
+        }
+
+        // GET: Clients/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var client = await _clientRepo.GetByIdAsync(id.Value);
+            if (client == null) return NotFound();
+
+            return View(client);
+        }
+
+        // GET: Clients/Create
+        public IActionResult Create() => View();
+
+        // POST: Clients/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,ContactDetails,Region")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                await _clientRepo.AddAsync(client);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(client);
+        }
+
+        // GET: Clients/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var client = await _clientRepo.GetByIdAsync(id.Value);
+            if (client == null) return NotFound();
+
+            return View(client);
+        }
+
+        // POST: Clients/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,ContactDetails,Region")] Client client)
+        {
+            if (id != client.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                await _clientRepo.UpdateAsync(client);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(client);
+        }
+
+        // GET: Clients/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var client = await _clientRepo.GetByIdAsync(id.Value);
+            if (client == null) return NotFound();
+
+            return View(client);
+        }
+
+        // POST: Clients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _clientRepo.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
