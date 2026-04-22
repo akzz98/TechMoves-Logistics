1a628fed398028afdff83f8c1d73b48c93c0523c
zulukho0 <zulukho0@gmail.com>
2026-04-22 10:17:19 +0200

    (File Clenup) Add HomeController and views for Index and Privacy pages, along with ErrorViewModel and launch settings

---
+++ TechMoves Logistics/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechMoves_Logistics.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
