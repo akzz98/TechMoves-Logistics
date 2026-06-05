f94d591b4eb078d5a4af403b227ea057dabcd39d
akzz98 <st10263456@myemeris.edu.za>
2026-06-05 12:46:44 +0200

    Remove EF Core and service registrations from MVC Program.cs

--- TechMoves Logistics/Program.cs
+++ TechMoves Logistics/Program.cs
@@ -1,32 +1,7 @@
-using Microsoft.EntityFrameworkCore;
-using TechMoveLogistics.Repositories;
-using TechMoveLogistics.Repositories.Interfaces;
-using TechMoves_Logistics.Data;
-using TechMoves_Logistics.Repositories;
-using TechMoves_Logistics.Repositories.Interfaces;
-using TechMoves_Logistics.Services;
-using TechMoves_Logistics.Services.Interfaces;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-// EF Core + SQL Server
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Repositories
-builder.Services.AddScoped<IClientRepository, ClientRepository>();
-builder.Services.AddScoped<IContractRepository, ContractRepository>();
-builder.Services.AddScoped<IServiceRequestRepository, ServiceRequestRepository>();
-
-// Services
-builder.Services.AddScoped<IContractService, ContractService>();
-builder.Services.AddScoped<IServiceRequestService, ServiceRequestService>();
-builder.Services.AddScoped<IFileService, FileService>();
-// HttpClient API
-builder.Services.AddHttpClient<ICurrencyService, CurrencyService>();
 
 var app = builder.Build();
 
