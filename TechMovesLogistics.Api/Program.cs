841297c9fe92007ee1d86e2aebbd609eeefa3acc
akzz98 <st10263456@myemeris.edu.za>
2026-06-05 11:25:54 +0200

    Add DbContext configuration for ApplicationDbContext in Program.cs

--- TechMovesLogistics.Api/Program.cs
+++ TechMovesLogistics.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TechMoves_Logistics.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// EF Core + SQL Server (API owns database access)
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
