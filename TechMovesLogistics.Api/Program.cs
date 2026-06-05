4e744e52639bd31307936d2b8c0ad6134c527b59
akzz98 <st10263456@myemeris.edu.za>
2026-06-05 12:43:00 +0200

    Refactor Program.cs: reorganize using directives and add service registrations for repositories and services

--- TechMovesLogistics.Api/Program.cs
+++ TechMovesLogistics.Api/Program.cs
@@ -1,17 +1,32 @@
 using Microsoft.EntityFrameworkCore;
+using TechMoveLogistics.Repositories;
+using TechMoveLogistics.Repositories.Interfaces;
 using TechMoves_Logistics.Data;
+using TechMoves_Logistics.Repositories;
+using TechMoves_Logistics.Repositories.Interfaces;
+using TechMoves_Logistics.Services;
+using TechMoves_Logistics.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 // EF Core + SQL Server (API owns database access)
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Repositories
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<IContractRepository, ContractRepository>();
+builder.Services.AddScoped<IServiceRequestRepository, ServiceRequestRepository>();
+
+// Services
+builder.Services.AddScoped<IContractService, ContractService>();
+builder.Services.AddScoped<IServiceRequestService, ServiceRequestService>();
+builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddHttpClient<ICurrencyService, CurrencyService>();
 
 var app = builder.Build();
 
