57aa3859da0ff2aa31aeebff8f177aac23c700f2
akzz98 <st10263456@myemeris.edu.za>
2026-06-08 11:21:31 +0200

    Add Swagger UI configuration and update project dependencies for API documentation

--- TechMovesLogistics.Api/Program.cs
+++ TechMovesLogistics.Api/Program.cs
@@ -11,7 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // EF Core + SQL Server (API owns database access)
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -33,7 +34,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "TechMoves Logistics API v1");
+        options.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
